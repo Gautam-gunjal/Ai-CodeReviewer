d54c6c14d34e909bac5517f01b0309feb3909a0a
Gautam Gunjalgit config --global user.email <gautam.2003.gunjal@gmail.com>
2026-04-11 23:25:19 +0530

    added healthRoute

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,12 +2,15 @@ const express=require("express");
 const app=express();
 const aiRoutes=require("./routes/ai.routes");
 const cors=require("cors");
+const healthRoute = require('./routes/health.route')
 
 app.use(cors({
     origin:"https://ai-code-reviewer-beryl-nine.vercel.app"
 }))
+
 app.use(express.json());
 app.use("/ai",aiRoutes);
+app.use('/health',healthRoute)
 
 
 module.exports=app
